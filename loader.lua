a465eddf01ca85aac6acec742b7344592a60ed5a
Agentotten <kasperalind@gmail.com>
2021-08-17 16:13:19 +0200

    Updated for Mixer

--- loader.lua
+++ loader.lua
@@ -1,20 +1,20 @@
 local Mixer = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Agentotten/mixer/main/mixer.lua"))()
 
-local ProjectFolder = Mixer:AddProject("AgentHub")
-local ScriptsFolder = ProjectFolder:AddFolder("Scripts")
-local GameScriptsFolder = ProjectFolder:AddFolder("GameScripts")
+local ProjectFolder = Mixer:Folder("AgentHub")
+local ScriptsFolder = ProjectFolder:Folder("Scripts")
+local GameScriptsFolder = ProjectFolder:Folder("GameScripts")
 
 local scripts = {}
 local gameScripts = {}
 
-for _, s in pairs(ScriptsFolder:GetChildren()) do
-	if s.Type == "file" then
+for _, s in pairs(ScriptsFolder:List()) do
+	if s.Type == "File" then
 		table.insert(scripts, s)
 	end
 end
 
-for _, gs in pairs(GameScriptsFolder:GetChildren()) do
-	if gs.Type == "file" then
+for _, gs in pairs(GameScriptsFolder:List()) do
+	if gs.Type == "File" then
 		table.insert(gameScripts, gs)
 	end
 end
